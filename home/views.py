45f6ca74d9a671e69e99a5451d701be5f2de4dab
Ty <satperson@mailfence.com>
2023-09-28 19:28:29 +0200

    Added. Welcome home message.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome home. Please make a choice.")
 
 # Create your views here.
